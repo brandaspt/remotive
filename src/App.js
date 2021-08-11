3f7f3a7f11ff6fec6e68655d39defe6e053ebb64
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-08-11 16:00:35 +0100

    Favorites View

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from "./views/Home"
 
 import "./App.css"
 import Company from "./views/Company"
+import Favourites from "./views/Favourites"
 
 const App = () => {
   return (
     <div className="App">
       <TopBar />
       <Route exact path="/" component={Home} />
+      <Route exact path="/favorites" component={Favourites} />
       <Route path="/companies/:company_name" component={Company} />
     </div>
   )
